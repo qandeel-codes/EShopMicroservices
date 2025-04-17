1e765e586fbc16b447b3731603335cc7a6df3f2c
Mohammad Qandeel <mohammad.qandeel@outlook.com>
2025-04-17 09:53:26 +0400

    Added paging to get products endpoint.

--- src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.Api/Products/GetProducts/GetProductsHandler.cs
@@ -1,8 +1,7 @@
 using Marten.Linq;
-
 namespace Catalog.Api.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 
 public record GetProductsResult(IEnumerable<Product> Products);
 
@@ -11,8 +10,9 @@ internal class GetProductsQueryHandler(IDocumentSession session)
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
-        
+        var products = await session.Query<Product>()
+            .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
+
         return new GetProductsResult(products);
     }
 }
