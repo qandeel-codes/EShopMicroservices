f33a4996b24989ed05ce2801f07d7a213c926822
Mohammad Qandeel <mohammad.qandeel@outlook.com>
2025-04-21 08:44:49 +0400

    Added basket microservice project and implement basket API handlers and endpoints.

---
+++ src/Services/Basket/Basket.Api/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,17 @@
+namespace Basket.Api.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+internal class GetBasketQueryHandler()
+    : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //TODO: get basket from database
+        //var basket = await _repository.GetBasket(query.UserName);
+        
+        return new GetBasketResult(new ShoppingCart("swn"));
+    }
+}
